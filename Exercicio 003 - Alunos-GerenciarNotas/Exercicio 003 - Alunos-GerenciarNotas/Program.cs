0c7a6e1c22cf12d87119ec8dc004a7f1a06901d6
andersongabsilva <Andersongabriel_maria@hotmail.com>
2020-02-25 18:19:14 -0300

    Exercicios POO
    Exercicios Alunos

---
+++ Exercicio 003 - Alunos-GerenciarNotas/Exercicio 003 - Alunos-GerenciarNotas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio_003___Alunos_GerenciarNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
